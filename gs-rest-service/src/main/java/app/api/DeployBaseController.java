d9785d62415eb09aec925ca4c58f42af4db1a065
kky <you@example.com>
2018-06-20 16:53:17 +0900

    pretty json 추가

--- gs-rest-service/src/main/java/app/api/DeployBaseController.java
+++ gs-rest-service/src/main/java/app/api/DeployBaseController.java
@@ -10,8 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 public abstract class DeployBaseController {
     private static final Logger contentLogger = LoggerFactory.getLogger("ContentLogger");
+    private static final Logger prettyLogger = LoggerFactory.getLogger("PrettyLogger");
     
 	protected abstract Logger getLogger();
 	protected abstract String getTag();
@@ -34,12 +40,14 @@ public abstract class DeployBaseController {
     	String uri = request.getRequestURI().toString();
     	getLogger().info(getTag() + "[" + uri + "]:" + in);
     	
-    	contentLogger.info(uri.substring(uri.indexOf("/v1")));
-    	contentLogger.info(in);
+    	String baseUrl = uri.substring(uri.indexOf("/v1"));
+
+    	log(baseUrl, in);
+    	logJson(baseUrl, in);
     	
     	return ("OK");
     }
-
+    
     @RequestMapping(
     		value= {"/**"}
     		, method = {RequestMethod.GET, RequestMethod.POST})
@@ -48,10 +56,27 @@ public abstract class DeployBaseController {
     	String uri = request.getRequestURI().toString();
     	getLogger().warn(getTag() + "[" + uri + "]:" + in);
     	
-    	contentLogger.info(uri.substring(uri.indexOf("/v1")));
-    	contentLogger.info(in);
+    	String baseUrl = uri.substring(uri.indexOf("/v1"));
+
+    	log(baseUrl, in);
+    	logJson(baseUrl, in);
     	
     	return ("OK");
     }
 
+    protected void log(String baseUrl, String in) {
+    	contentLogger.info(baseUrl);
+    	contentLogger.info(in);
+    }
+    
+    protected void logJson(String baseUrl, String json) {
+    	JsonParser jp = new JsonParser();
+    	JsonElement je = jp.parse(json);
+    	
+    	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    	String prettyJson = gson.toJson(je);
+
+    	prettyLogger.info(baseUrl);
+    	prettyLogger.info(prettyJson);
+    }
 }
